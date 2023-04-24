e5217a5907caa25a3d5cf3a736632ecdd491e798
unknown <kanishkamdilhan@gmail.com>
2023-04-24 16:38:02 +0530

    change workflow file to show discriptive test result

--- src/fetchInfo.js
+++ src/fetchInfo.js
@@ -6,17 +6,14 @@ export async function fetchInfo() {
     params: { q: "6.94,79.85" },
     headers: {
       "X-RapidAPI-Key": process.env.REACT_APP_X_RAPIDAPI_KEY,
-      // "X-RapidAPI-Key": "f53cd4b26emsh35ad5403ea1370ep1664bfjsnfad2ee08906f",
       "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
     },
   };
 
   try {
     const response = await axios.request(options); // wait for the response
-    // console.log(response.data);
     return response.data.current.feelslike_c;
   } catch (error) {
-    // console.error(error);
     return error;
   }
 }
--- src/fetchInfo.test.js
+++ src/fetchInfo.test.js
@@ -17,7 +17,6 @@ describe('fetchInfo', () => {
         params: { q: '6.94,79.85' },
         headers: {
           'X-RapidAPI-Key': process.env.REACT_APP_X_RAPIDAPI_KEY,
-          // "X-RapidAPI-Key": "f53cd4b26emsh35ad5403ea1370ep1664bfjsnfad2ee08906f",
           'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
         },
       });
