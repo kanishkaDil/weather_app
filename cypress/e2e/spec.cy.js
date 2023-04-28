20e70077fd061fbd9c86e64667bff133d6348ff5
unknown <kanishkamdilhan@gmail.com>
2023-04-28 12:17:11 +0530

    workflow update for cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,10 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
-    cy.contains('Git').click()
+    // cy.contains('Git').click()
     // cy.url().should('include', 'https://github.com/kanishkaDil/weather_app')
+    cy.contains(3)
+    // cy.contains(/^\d+$/)
+    // cy.contains(/^([2-9][0-9]|[1-9][0-9]{2}|100)(\.[0]{1,2})?$/)
   })
 })
